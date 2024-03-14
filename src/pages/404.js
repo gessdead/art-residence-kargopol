490ce3021cb0bf5da9c0f28131f13a9a53fa73d6
gessdead <s1991gv@gmail.com>
2024-03-14 23:31:43 +0300

    - del unused pages && customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Container, Typography } from "@mui/material"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container maxWidth='lg' sx={{ paddingTop: "70px" }}>
+      <Typography variant="h2" align="center" fontFamily='var(--font-sans)'>
+        Такой страницы нет 😢
+      </Typography>
+      <br/>
+      <Typography variant="h4" align="center" fontFamily="var(--font-sans)">
+        Вернемся  <Link to={'/'}>домой</Link>?
+      </Typography>
+    </Container>
   </Layout>
 )
 
